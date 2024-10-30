44d9b2db4ebe64e33b4b48abdb40ce4761e7ef37
starsden <159563356+starsden@users.noreply.github.com>
2024-10-30 22:19:16 +0300

    add favicon, cosmetic fixed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import os
+
 
 app = FastAPI(
     docs_url=None,
@@ -10,6 +11,8 @@ app = FastAPI(
 )
 
 
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/fonts", StaticFiles(directory="./fonts"), name="fonts")
 templates = Jinja2Templates(directory="./templates")
 
