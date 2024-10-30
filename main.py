2a30380dfba2ab79d9e8200d22bafd87796c6521
starsden <159563356+starsden@users.noreply.github.com>
2024-10-30 18:22:18 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI()
+
+app.mount("/fonts", StaticFiles(directory="./fonts"), name="fonts")
+templates = Jinja2Templates(directory="./templates")
+
+@app.get("/")
+async def get_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/submit")
+async def submit_email(email: str = Form(...)):
+    with open("emails.txt", "a") as file:
+        file.write(email + "\n")
+    return RedirectResponse("/", status_code=303)
+
